350a8f4efcb75c509e2e1240dfecd10720f391ea
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-01-25 16:38:22 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,9 @@
+// Task 1 Working With Arrays
+let products = ["Phone", "Mouse", "Laptop", "Headset", "Desktop Computer"];
+
+products.push("Webcam");
+
+products.pop();
+
+console.log("Updated List of Available Products:", products);
+
